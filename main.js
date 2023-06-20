b1ca8d833e22a2be26d88d2a83d158c2fd85874f
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-06-20 10:23:19 +0200

    changed JS code to language switcher

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ import './style.css'
 
 var userLang = navigator.language || navigator.userLanguage
 
-// Проверка языка и перенаправление на соответствующую страницу
-if (userLang.startsWith('ru')) {
-  window.location.href = 'index.html'
-} else {
-  window.location.href = 'english.html'
+// Проверка текущего URL
+var currentURL = window.location.href
+
+// Проверка, нужно ли выполнить переадресацию
+if (
+  !currentURL.includes('index.html') &&
+  !currentURL.includes('english.html')
+) {
+  // Проверка языка и переадресация на соответствующую страницу
+  if (userLang.startsWith('ru')) {
+    window.location.href = 'index.html'
+  } else {
+    window.location.href = 'english.html'
+  }
 }
 
 // AOS animation
