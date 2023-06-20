86d0f67cdce257e5d3b5ec12bcc18d5987cc54e5
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-06-20 16:37:29 +0200

    added english version

--- main.js
+++ main.js
@@ -26,3 +26,60 @@ window.onload = function () {
 
 // AOS animation
 AOS.init()
+
+// НАЖАТИЕ НА КНОПКУ ПОДРОБНЕЕ
+
+// найдем кнопки ПОДРОБНЕЕ на мобилке и компе
+let mobFirstBtn = document.querySelector('#mobFirst')
+let deskFirstBtn = document.querySelector('#deskFirst')
+
+let mobSecondBtn = document.querySelector('#mobSecond')
+let deskSecondBtn = document.querySelector('#deskSecond')
+
+let mobThirdBtn = document.querySelector('#mobThird')
+let deskThirdBtn = document.querySelector('#deskThird')
+
+// найдем секцию до куда надо прокрутить на мобилке и компе
+let citySectionMob = document.querySelector('#citySectionMob')
+let citySectionDesk = document.querySelector('#citySectionDesk')
+
+let marketSectionMob = document.querySelector('#marketSectionMob')
+let marketSectionDesk = document.querySelector('#marketSectionDesk')
+
+let homesSectionMob = document.querySelector('#homesSectionMob')
+let homesSectionDesk = document.querySelector('#homesSectionDesk')
+
+// функционал для мобилки
+function scrollTo(element) {
+  window.scroll({
+    left: 0,
+    top: element.offsetTop,
+    behavior: 'smooth',
+  })
+}
+// ДЛЯ 1 КНОПКИ
+mobFirstBtn.addEventListener('click', () => {
+  scrollTo(citySectionMob)
+})
+// функционал для компа
+deskFirstBtn.addEventListener('click', () => {
+  scrollTo(citySectionDesk)
+})
+
+// ДЛЯ 2 КНОПКИ
+mobSecondBtn.addEventListener('click', () => {
+  scrollTo(marketSectionMob)
+})
+// функционал для компа
+deskSecondBtn.addEventListener('click', () => {
+  scrollTo(marketSectionDesk)
+})
+
+// ДЛЯ 3 КНОПКИ
+mobThirdBtn.addEventListener('click', () => {
+  scrollTo(homesSectionMob)
+})
+// функционал для компа
+deskThirdBtn.addEventListener('click', () => {
+  scrollTo(homesSectionDesk)
+})
