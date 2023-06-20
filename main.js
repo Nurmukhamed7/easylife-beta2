f0138cccad7cd203927b88877691658ef1944c25
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-06-20 11:19:24 +0200

    added new code in JS to language switcher

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 import './style.css'
 
-var userLang = navigator.language || navigator.userLanguage
+window.onload = function () {
+  var userLang = navigator.language || navigator.userLanguage
+  var currentPage = window.location.pathname
 
-// Проверка текущего URL
-var currentURL = window.location.href
-
-// Проверка, нужно ли выполнить переадресацию
-if (
-  !currentURL.includes('index.html') &&
-  !currentURL.includes('english.html')
-) {
-  // Проверка языка и переадресация на соответствующую страницу
-  if (userLang.startsWith('ru')) {
+  if (userLang.startsWith('ru') && currentPage !== '/index.html') {
     window.location.href = 'index.html'
-  } else {
+  } else if (userLang.startsWith('en') && currentPage !== '/english.html') {
     window.location.href = 'english.html'
   }
 }
