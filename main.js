9bd8060828e27b5e11443d850bfc54f23f7dfca3
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-06-20 12:02:37 +0200

    changed js time 12:02

--- main.js
+++ main.js
@@ -3,11 +3,24 @@ import './style.css'
 window.onload = function () {
   var userLang = navigator.language || navigator.userLanguage
   var currentPage = window.location.pathname
+  var langSwitch = sessionStorage.getItem('langSwitch')
 
-  if (userLang.startsWith('ru') && currentPage !== '/index.html') {
-    window.location.href = 'index.html'
-  } else if (userLang.startsWith('en') && currentPage !== '/english.html') {
+  if (!langSwitch) {
+    if (userLang.startsWith('ru') && currentPage !== '/index.html') {
+      window.location.href = 'index.html'
+    } else if (userLang.startsWith('en') && currentPage !== '/english.html') {
+      window.location.href = 'english.html'
+    }
+  }
+
+  document.getElementById('switchToEnglish').onclick = function () {
+    sessionStorage.setItem('langSwitch', 'true')
     window.location.href = 'english.html'
+  }
+
+  document.getElementById('switchToRussian').onclick = function () {
+    sessionStorage.setItem('langSwitch', 'true')
+    window.location.href = 'index.html'
   }
 }
 
