43027e7eafab19e5829c473319a19bad420d07b0
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-06-20 10:06:11 +0200

    removed modal and added language switcher

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 import './style.css'
 
-// Закрытие модального окна по клику на крестик или вне модального окна
-const modal = document.getElementById('modal')
-const closeModalButton = document.getElementById('closeModal')
+var userLang = navigator.language || navigator.userLanguage
 
-closeModalButton.addEventListener('click', () => {
-  modal.classList.add('hidden')
-})
-
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.classList.add('hidden')
-  }
-})
-
-// Задержка появления модального окна
-setTimeout(() => {
-  modal.classList.remove('hidden')
-}, 1000)
+// Проверка языка и перенаправление на соответствующую страницу
+if (userLang.startsWith('ru')) {
+  window.location.href = 'index.html'
+} else {
+  window.location.href = 'english.html'
+}
 
 // AOS animation
 AOS.init()
